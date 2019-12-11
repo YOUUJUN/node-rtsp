24807bbdd23e5f82cc398140d55a626f6e2c2ffb
YOUUJUN <824000803@qq.com>
2019-12-11 16:16:18 +0800

    init

--- controler/home.js
+++ controler/home.js
@@ -7,19 +7,23 @@ const fsPromises = require('fs').promises;
 const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 
+const streamAPI = require("node-rtsp-stream");
+
+const stream = new streamAPI({
+    name: 'name',
+    streamUrl: 'rtsp://admin:admin123@192.168.3.38:554/MPGE-1/ch34/main/av_stream',
+    wsPort: 9999,
+    ffmpegOptions: { // options ffmpeg flags
+        '-stats': '', // an option with no neccessary value uses a blank string
+        '-r': 30 // options with required values specify the value after the key
+    }
+});
+
+
 module.exports = {
     index : async (ctx,next) =>{
-        const username = ctx.cookies.get("username");
 
-        const ifExist = await workspaces.checkWorkSpaceByUser(username);
-
-
-        if(!ifExist){
-            let workName = workspaces.workspaceNameGenerator(username);
-            let userPath = await workspaces.createWorkSpace("F:\\learn_git_koa\\public\\source",workName);
-        }
-
-        await next();
+        await ctx.render("index.html");
     },
 
     getFile : async (ctx,next) => {
